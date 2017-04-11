d340fb4e1921ed1ee69fa534320cc1eea241c771
plantlog-jcahalan <jcahalan@plantlog.com>
2017-04-10 21:08:22 -0700

    added decoration to the selected statement

--- src/controllers/RequestController.ts
+++ src/controllers/RequestController.ts
@@ -1,4 +1,4 @@
-import {window, workspace, commands, Range, TextEditor, Disposable, Uri, TextDocument, ViewColumn} from 'vscode';
+import {window, workspace, commands, Range, TextEditor, Disposable, Uri, TextDocument, ViewColumn, TextEditorDecorationType} from 'vscode';
 import {ConnectionController} from './ConnectionController';
 import {MySqlResultDocumentContentProvider} from '../views/MySqlResultDocumentContentProvider';
 import {MySqlStatementParser} from '../utils/MySqlStatementParser';
@@ -10,39 +10,41 @@ export class RequestController{
 
     constructor(){
         this._resultDocumentProvider = new MySqlResultDocumentContentProvider();
-        this._resultDocumentRegistration = workspace.registerTextDocumentContentProvider('mysql-scratchpad', this._resultDocumentProvider)
+        this._resultDocumentRegistration = workspace.registerTextDocumentContentProvider('mysql-scratchpad', this._resultDocumentProvider);
     }
 
-    public executeStatementUnderCursor(){
-        let editor = window.activeTextEditor;
+    public executeStatementUnderCursor(editor:TextEditor){
         if(!editor || !editor.document){
             return;
         }
         let parser = new MySqlStatementParser(editor);
-        let statement = parser.parseStatementUnderCursor();
-        
-        if(!statement){
+        let parsed = parser.parseStatementAndRangeUnderCursor();
+
+        this.updateDecorations(editor, parsed.range);
+
+        if(!parsed || !parsed.statement){
             return;
         }
-        this._resultDocumentProvider.setStatement(statement);
-        this.execute(statement)
-            .then(result => {
-                OutputChannelController.outputSuccesss({
-                    statement:statement,
-                    message:result.message
-                });
-                this._resultDocumentProvider.setResult(result);
-                let uri:Uri = Uri.parse('mysql-scratchpad://authority/result');
-                this._resultDocumentProvider.refresh(uri);
-                commands.executeCommand('vscode.previewHtml', uri, ViewColumn.Two, 'MySQL Result');
-            }, (error) => {
-                window.showErrorMessage(error.message);
-                OutputChannelController.outputError({
-                    message: error.message,
-                    statement: statement
-                })
-            });
+        this._resultDocumentProvider.setStatement(parsed.statement);
+
+        this.execute(parsed.statement)
+            .then(result => this.onExecutionSuccess(result, parsed.statement), 
+                (error) => this.onExecutionError(error, parsed.statement));
         
+    }
+
+    private updateDecorations(editor:TextEditor, range:Range){
+        let options = {
+                light:{
+                    backgroundColor:`rgba(0,255,0,0.4)`
+                },
+                dark: {
+                    backgroundColor:`rgba(255,255,255,0.4)`
+                }
+            };
+        let decoType = window.createTextEditorDecorationType(options);
+        editor.setDecorations(decoType,[range]);
+        setTimeout(() => editor.setDecorations(decoType,[]), 1000);
     }
 
     private execute(sql:string, args?:any[]):Thenable<any>{
@@ -59,6 +61,25 @@ export class RequestController{
                 reject({message: 'No Connection'});
             }
         });
+    }
+
+    private onExecutionSuccess(result, statement){
+        OutputChannelController.outputSuccesss({
+            statement:statement,
+            message:result.message
+        });
+        this._resultDocumentProvider.setResult(result);
+        let uri:Uri = Uri.parse('mysql-scratchpad://authority/result');
+        this._resultDocumentProvider.refresh(uri);
+        commands.executeCommand('vscode.previewHtml', uri, ViewColumn.Two, 'MySQL Result');
+    }
+
+    private onExecutionError(error, statement){
+        window.showErrorMessage(error.message);
+        OutputChannelController.outputError({
+            message: error.message,
+            statement: statement
+        })
     }
 
     public dispose(){
