c23aa1d664f39e616e68925cb7c85e102cad53dd
chubtoad-black <jaredcahalan@gmail.com>
2017-04-27 17:09:30 -0700

    created options for the connection gathering prompts there are now options that will affect what the user is prompted for when connecting to a server

--- src/utils/MySQLUtil.ts
+++ src/utils/MySQLUtil.ts
@@ -5,44 +5,82 @@ export class MySQLUtil{
 
     public static getMysqlConnectionOptions():Promise<ConnectionOptions>{
         let options:ConnectionOptions = {};
+        let extensionConfig = vscode.workspace.getConfiguration('mysql-scratchpad');
+        
         return this.getHostName(options)
+            .then(options => this.getPort(options, extensionConfig))
             .then(options => this.getUserName(options))
             .then(options => this.getUserPass(options))
+            .then(options => this.getDatabase(options, extensionConfig))
             .then(options => this.addDefaults(options));
     }
 
-    private static getHostName(options:ConnectionOptions):Promise<ConnectionOptions>{
-        return new Promise<ConnectionOptions>((resolve, reject) => {
-            vscode.window.showInputBox({
-                prompt: "MySQL host:"
-            }).then(hostName => {
-                options.host = hostName;
-                resolve(options);
-            });
+    private static getOptionField(promptOptions:any, addInputToOptions:(inputString:string)=>ConnectionOptions){
+        return new Promise<ConnectionOptions>(resolve => {
+            vscode.window.showInputBox(promptOptions)
+                            .then(inputString => resolve(addInputToOptions(inputString)));
         });
+    }
+
+    private static getHostName(options:ConnectionOptions):Promise<ConnectionOptions>{
+        return this.getOptionField({
+                prompt: "MySQL Host"}, 
+            hostName => {
+                options.host = hostName;
+                return options;   
+            });
     }
 
     private static getUserName(options:ConnectionOptions):Promise<ConnectionOptions>{
-        return new Promise<ConnectionOptions>((resolve, reject) => {
-            vscode.window.showInputBox({
-                prompt:'Username for '+options.host,
-            }).then(userName => {
+        return this.getOptionField({
+                prompt:'Username for '+options.host},
+            userName => {
                 options.user = userName;
-                resolve(options);
-            });
-        });
+                return options;
+            }
+        )
     }
 
     private static getUserPass(options:ConnectionOptions):Promise<ConnectionOptions>{
-        return new Promise<ConnectionOptions>((resolve, reject) => {
-            vscode.window.showInputBox({
+        return this.getOptionField({
                 prompt:'Password for '+options.user+'@'+options.host,
                 password:true
-            }).then(userPass => {
+            },
+            userPass => {
                 options.password = userPass;
-                resolve(options);
-            });
-        });
+                return options;
+            }
+        );
+    }
+
+    private static getPort(options:ConnectionOptions, extensionConfig):Promise<ConnectionOptions>{
+        if(extensionConfig.get("promptForPort")){
+            return this.getOptionField({
+                    prompt:'MySQL Port'
+                },
+                port => {
+                    options.port = Number(port);
+                    return options;
+                });
+        }else{
+            let defaultPort:number = extensionConfig.get("defaultMysqlPort");
+            options.port = defaultPort;
+            return new Promise<ConnectionOptions>(resolve => resolve(options));
+        }
+    }
+
+    private static getDatabase(options:ConnectionOptions, extensionConfig):Promise<ConnectionOptions>{
+        if(extensionConfig.get("promptForDatabase")){
+            return this.getOptionField({
+                    prompt: 'Database Name'
+                },
+                dbName => {
+                    options.database = dbName;
+                    return options
+                });
+        }else{
+            return new Promise<ConnectionOptions>(resolve => resolve(options));
+        }
     }
 
     private static addDefaults(options:ConnectionOptions):Promise<ConnectionOptions>{
