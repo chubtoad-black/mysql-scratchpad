f0f6088c6e5f79fc3be44b89d31aa65ea20c4430
chubtoad-black <jaredcahalan@gmail.com>
2017-05-03 16:47:06 -0700

    made the result store have a limit on the amount of results it will cache also added a setting to change the liit

--- src/utils/ResultStore.ts
+++ src/utils/ResultStore.ts
@@ -1,3 +1,4 @@
+import * as vscode from 'vscode';
 
 export interface MySQLResult{
     statement:string,
@@ -7,13 +8,22 @@ export interface MySQLResult{
 }
 
 export class ResultStore{
-
+    
+    private static CACHE_SIZE = vscode.workspace.getConfiguration('mysql-scratchpad').get<number>('resultCacheSize') | 10;
     private static store:Map<string, MySQLResult> = new Map<string, MySQLResult>();
+    private static cacheWindow:Array<string> = new Array<string>();
     private static nth:number = 0;
 
-    public static add(uri:string, result:MySQLResult){
+    public static add(uri:string, result:MySQLResult):string{
         this.store.set(uri, result);
         this.nth++;
+        this.cacheWindow.push(uri);
+        let toRemove = null;
+        if(this.cacheWindow.length > this.CACHE_SIZE){
+            toRemove = this.cacheWindow.shift();
+            this.remove(toRemove);
+        }
+        return toRemove;
     }
 
     public static count(){
@@ -24,14 +34,15 @@ export class ResultStore{
         return this.store.get(uri);
     }
 
-    public static remove(uri:string){
-        if(this.store.has(uri)){
-            this.store.delete(uri);
-        }
-    }
 
     public static clear(){
         this.store.clear();
         this.nth = 0;
+    }
+
+    private static remove(uri:string){
+        if(this.store.has(uri)){
+            this.store.delete(uri);
+        }
     }
 }
