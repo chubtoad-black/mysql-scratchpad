c0b2592c72afde011a1a91e5abf5eb679acd21c3
chubtoad-black <jaredcahalan@gmail.com>
2017-05-07 18:04:08 -0700

    added a timestmap to the output channel

--- src/controllers/OutputChannelController.ts
+++ src/controllers/OutputChannelController.ts
@@ -22,25 +22,38 @@ export class OutputChannelController{
         OutputChannelController.outputChannel.show();
     }
 
+    private static timestamp():string{
+        let pad = (num:number):string => {
+            let out = num.toString();
+            if(out.length < 2){
+                return '0'+out
+            }
+            return out;
+        }
+
+        let now = new Date();
+        return `[${now.getFullYear()}-${pad(now.getMonth()+1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}] - `
+    }
+
     public static outputConnection(connection:Connection){
-        let line = "Connected as "+connection.config.user+"@"+connection.config.host;
+        let line = `${this.timestamp()}Connected as ${connection.config.user}@${connection.config.host}`;
         OutputChannelController.outputChannel.appendLine(line);
     }
 
     public static outputDisconnect(){
-        let line = "Disconnected from MySQL server.";
+        let line = `${this.timestamp()}Disconnected from MySQL server.`;
         OutputChannelController.outputChannel.appendLine(line);
     }
 
     public static outputError(error:Error){
-        let line = "Statement: "+error.statement+"   Error: "+error.message;
+        let line = `${this.timestamp()}Statement: ${error.statement}   Error: ${error.message}`;
         OutputChannelController.outputChannel.appendLine(line);
     }
     
     public static outputSuccesss(result:Result){
-        let line = "Statement: "+result.statement;
+        let line = `${this.timestamp()}Statement: ${result.statement}`;
         if(result.message){
-            line +="     Message: "+result.message;
+            line +=`     Message: ${result.message}`;
         }
         OutputChannelController.outputChannel.appendLine(line);
     }
